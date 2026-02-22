d0a0291629874d8c3fddb4ad7b614d0dab4e8667
Gianni Ivan Zeniquel <giannizeniquel25@outlook.com>
2026-02-22 14:20:47 -0300

    feat: configure Space Grotesk for headings and IBM Plex Sans for body in docs

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -6,7 +6,10 @@ export default defineConfig({
   appearance: 'dark',
 
   head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600&family=JetBrains+Mono:wght@400&family=Space+Grotesk:wght@400;600&display=swap' }]
   ],
 
   locales: {
